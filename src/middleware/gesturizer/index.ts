a9c7feb3f93a7a51a80000988c9a93394cd47139
Paul Brachmann <mail@paulbrachmann.com>
2020-02-27 19:34:34 +0100

    Implement TransformGesture

--- src/middleware/gesturizer/index.ts
+++ src/middleware/gesturizer/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 
 export { default as TransformData } from "./transformData";
+export { default as TransformGesture } from "./transformGesture";
---
+++ src/middleware/gesturizer/transformGesture.ts
@@ -0,0 +1,50 @@
+import TransformData from "./transformData";
+
+/**
+ * Transform gesture, used to compute the offset transformation
+ * from the transformation origin to the gesture's target.
+ */
+class TransformGesture {
+  /** Current gesture target. */
+  protected target: TransformData = new TransformData();
+
+  /** Static offset for internal corrections. */
+  protected offset: TransformData = new TransformData();
+
+  constructor(
+    /** The initial gesture target. */
+    protected origin: TransformData = new TransformData(),
+    /** Gesture context (to store arbitrary data related to the gesture). */
+    public context: { [key: string]: any } = {},
+  ) {
+    this.target.set(origin);
+  }
+
+  /** Returns the transformation data resulting from the gesture. */
+  public getTransform(): TransformData {
+    return this.target
+      .clone()
+      .subtract(this.origin)
+      .add(this.offset);
+  }
+
+  /**
+   * Replaces the origin (initial transformation data),
+   * preserves resulting transformation.
+   *
+   * @param origin The new origin
+   */
+  public rebase(origin: TransformData): TransformGesture {
+    this.offset.set(this.getTransform());
+    this.origin = origin;
+    return this.setTarget(origin);
+  }
+
+  /** Updates the gesture's target. */
+  public setTarget(currentTarget: Partial<TransformData>): TransformGesture {
+    this.target.set(currentTarget);
+    return this;
+  }
+}
+
+export default TransformGesture;
